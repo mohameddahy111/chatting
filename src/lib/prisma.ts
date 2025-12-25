c02bfc93d4c090b6edf907fcf3eaa5be5bd8b530
mohameddahy <mohameddahy111@gmail.com>
2025-12-25 03:42:24 +0200

    prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,10 @@
-import "dotenv/config";
+
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaClient } from "../../generated/prisma/client.ts";
 
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString =
+  "postgresql://postgres.uvmdmrgjjealzaidjyjr:224466aa!@aws-1-eu-west-1.pooler.supabase.com:5432/postgres";
 
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
